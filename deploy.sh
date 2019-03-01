4883ef5168216417496e732e45b8bd80f71ebd4e
Erik Westlund <edbwestlund@gmail.com>
2019-03-01 15:28:36 -0600

    ok

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,28 @@
 
 cd /var/www/blackjacker.net
 git pull origin master
+
+# build and go up once so we can grab files out of the container
 docker-compose -f docker-compose.prod.yml build
+docker-compose -f docker-compose.prod.yml up -d
 
+# hacky, but grab needed files out of container so we can serve in nginx directly
+# proxying, nginx into docker, these don't get served.
 CONTAINER=$(docker ps -q --filter="NAME=blackjacker")
-mkdir -p /var/www/blackjacker.net/app/www/__assets__/
-docker cp $CONTAINER:/usr/local/lib/R/site-library/shiny/www/shared/ /var/www/blackjacker.net/app/www/
-docker cp $CONTAINER:/opt/shiny-server/assets/ /var/www/blackjacker.net/app/www/__assets__/
-docker cp $CONTAINER:/opt/shiny-server/node_modules/shiny-server-client/dist/shiny-server-client.min.js /var/www/blackjacker.net/app/www/__assets__/
+SHINYWWW=/var/www/blackjacker.net/app/www
+DUNDERASS=$SHINYWWW/__assets__/
+SHAREDASS=$SHINYWWW/assets/shared/
 
+#remove shared assets and recopy
+rm -rf $SHAREDASS
+docker cp $CONTAINER:/usr/local/lib/R/site-library/shiny/www/shared/ $SHINYWWW/
+
+# remove dunderbar shared assets and recopy
+rm -rf $DUNDERASS
+docker cp $CONTAINER:/opt/shiny-server/assets/ $DUNDERASS
+
+# add import websocket stuff
+docker cp $CONTAINER:/opt/shiny-server/node_modules/shiny-server-client/dist/shiny-server-client.min.js $DUNDERASS/
+
+docker-compose -f docker-compose.prod.yml build
 docker-compose -f docker-compose.prod.yml up -d
