e6ae02e2819fb12dd3b481603a7d99bb374d4783
Erik Westlund <edbwestlund@gmail.com>
2019-03-01 14:28:51 -0600

    ugh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,11 @@
 cd /var/www/blackjacker.net
 git pull origin master
 docker-compose -f docker-compose.prod.yml build
+
+CONTAINER=$(docker ps -q --filter="NAME=blackjacker")
+mkdir -p /var/www/blackjacker.net/app/www/__assets__/
+docker cp $CONTAINER:/usr/local/lib/R/site-library/shiny/www/shared/ /var/www/blackjacker.net/app/www/
+docker cp $CONTAINER:/opt/shiny-server/assets/ /var/www/blackjacker.net/app/www/__assets__/
+docker cp $CONTAINER:/opt/shiny-server/node_modules/shiny-server-client/dist/shiny-server-client.min.js /var/www/blackjacker.net/app/www/__assets__/
+
 docker-compose -f docker-compose.prod.yml up -d
